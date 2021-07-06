85d63992ee752d3c893a292cf68eac199483d4ac
ATErickson <erickson@tc.columbia.edu>
2021-07-06 14:58:45 -0400

    Update google-doc-url.js
    Revert to HandonDataViz API

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fUCO0vq1_YcMVM81BtRNxFBk9Gl38gtzlu4YQmF21T0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBbJ-Y1JC1fCWsBr62xGjc1qbXfpblOIa4';
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
